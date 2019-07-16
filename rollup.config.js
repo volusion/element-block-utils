02b66a6b10553c6dcbe20eaf8d3d1e21081c9f29
Cameron Jones <cameron.jones@volusion.com>
2019-07-16 13:38:34 -0500

    chore: cleanup rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,37 +4,14 @@ import pkg from './package.json'
 export default {
   input: 'src/index.js',
   output: {
-      file: pkg.main,
-      name: 'blockUtils',
-      format: 'umd',
+    file: pkg.main,
+    name: 'blockUtils',
+    format: 'umd',
   },
-  // output: [
-  //   {
-  //     file: pkg.main,
-  //     format: 'umd',
-  //   },
-  //   {
-  //     file: pkg.module,
-  //     format: 'es',
-  //   },
-  // ],
-
-        plugins: [
-            babel({
-                // babelrc: false,
-                // presets: [['env', { modules: false }], 'stage-1', 'react'],
-                // externalHelpers: true,
-                exclude: ['node_modules/**']
-            }),
-            // resolve({
-            //     browser: true
-            // }),
-            // commonjs(),
-            // options.minify && uglify()
-        ]
-  // external: [
-  //   ...Object.keys(pkg.dependencies || {}),
-  //   ...Object.keys(pkg.peerDependencies || {}),
-  // ],
+  plugins: [
+    babel({
+      exclude: ['node_modules/**']
+    }),
+  ]
 }
 
