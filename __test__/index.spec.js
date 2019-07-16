00ad3b4b04d85190d14d46a59cf36dcc40b08fca
Cameron Jones <cameron.jones@volusion.com>
2019-07-16 11:39:36 -0500

    refactor: delete reduendant test code

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -4,23 +4,18 @@ import { StyleSheet, css } from 'aphrodite';
 import { createFactory } from 'src/index.js';
 
 describe('createFactory()', () => {
-    it('should run without errors', () => {
-        const blockFactory = React => () => <div />;
-        const styles = jest.fn();
-        const configSchema = jest.fn();
-        createFactory(blockFactory, styles, configSchema);
-    });
-
     it('should return a working React component', () => {
         const blockFactory = React => () => <div />;
-        const styles = jest.fn(() => ({}));
-        const configSchema = jest.fn();
+        const styles = () => ({});
+        const configSchema = () => {};
         const factory = createFactory(blockFactory, styles, configSchema);
+
         const TestBlock = factory(
             { React, Components: {} },
             {},
             { StyleSheet, css }
         ).block;
+
         const div = document.createElement('div');
         ReactDOM.render(<TestBlock />, div);
         ReactDOM.unmountComponentAtNode(div);
