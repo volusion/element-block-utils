b85f4eebce241670856086c4e550e680c342f109
Cameron Jones <cameron.jones@volusion.com>
2019-07-16 11:38:01 -0500

    test(createFactory): it produces a working block

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { StyleSheet, css } from 'aphrodite';
 import { createFactory } from 'src/index.js';
 
 describe('createFactory()', () => {
@@ -6,5 +9,20 @@ describe('createFactory()', () => {
         const styles = jest.fn();
         const configSchema = jest.fn();
         createFactory(blockFactory, styles, configSchema);
+    });
+
+    it('should return a working React component', () => {
+        const blockFactory = React => () => <div />;
+        const styles = jest.fn(() => ({}));
+        const configSchema = jest.fn();
+        const factory = createFactory(blockFactory, styles, configSchema);
+        const TestBlock = factory(
+            { React, Components: {} },
+            {},
+            { StyleSheet, css }
+        ).block;
+        const div = document.createElement('div');
+        ReactDOM.render(<TestBlock />, div);
+        ReactDOM.unmountComponentAtNode(div);
     });
 });
