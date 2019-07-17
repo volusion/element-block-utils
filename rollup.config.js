b02aad34c07f8c5087e55d233d8dab71287f4ecd
Cameron Jones <cameron.jones@volusion.com>
2019-07-17 17:12:41 -0500

    chore: lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,16 @@
 import babel from 'rollup-plugin-babel';
-import pkg from './package.json'
+import pkg from './package.json';
 
 export default {
-  input: 'src/index.js',
-  output: {
-    file: pkg.main,
-    name: 'blockUtils',
-    format: 'umd',
-  },
-  plugins: [
-    babel({
-      exclude: ['node_modules/**']
-    }),
-  ]
-}
-
+    input: 'src/index.js',
+    output: {
+        file: pkg.main,
+        name: 'blockUtils',
+        format: 'umd'
+    },
+    plugins: [
+        babel({
+            exclude: ['node_modules/**']
+        })
+    ]
+};
