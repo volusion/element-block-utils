4e5a4eb448635b7f4a307b4df546d7bebae4301a
Avery Milandin <averymilandin@gmail.com>
2022-01-11 16:08:10 -0800

    fix lint

--- src/mockUtils.js
+++ src/mockUtils.js
@@ -118,14 +118,14 @@ export const mockUtils = {
             text: 'Logo',
             publicId: '',
             url:
-                'https://res.cloudinary.com/dyx4yhvoq/image/upload/v1579715133/5d0bd834bd7d6e2d83cac6bc/fx3jlhu4lv3ispnumuap.png',
+                'https://res.cloudinary.com/dyx4yhvoq/image/upload/v1579715133/5d0bd834bd7d6e2d83cac6bc/fx3jlhu4lv3ispnumuap.png'
         },
         seo: {
             defaultDescription: '',
             defaultOverrideMetaTags: '',
             defaultTitle: '',
             globalMetaTags: ''
-        },
+        }
     },
     DEPRECATED_getLegacyButton: () => ({
         buttonStyle = 'primary',
